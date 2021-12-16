460eb2e3fd4f9c798351aa8b08c9b3fa4b145747
unknown <denissmirnov.rsatu@gmail.com>
2021-12-16 18:14:28 +0300

    first

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1 {
+        private System.ComponentModel.IContainer components = null;
+
+        System.Windows.Forms.Button[] myBtn = new System.Windows.Forms.Button[10];
+
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 
+        private void InitializeComponent() {
+            int pointButton = 0;
+            for (int i = 0; i < 10; i++) {
+                this.myBtn[i] = new System.Windows.Forms.Button();
+                this.myBtn[i].Location = new System.Drawing.Point(12 + pointButton, 67);
+                this.myBtn[i].Name = "button" + (i + 1).ToString();
+                this.myBtn[i].Size = new System.Drawing.Size(50, 40);
+                this.myBtn[i].TabIndex = 0;
+                this.myBtn[i].Text =  (i + 1).ToString();
+                this.myBtn[i].UseVisualStyleBackColor = true;
+                this.myBtn[i].Click += this.buttonMove;
+                pointButton += 75;
+            }
+
+            this.SuspendLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+
+            for (int i = 0; i < 10; i++) {
+                this.Controls.Add(this.myBtn[i]);
+            }
+
+            this.Name = "Form1";
+            this.Text = "LR_6";
+            this.ResumeLayout(false);
+        }
+        #endregion
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form 
+    {
+        public Form1() 
+        {
+            InitializeComponent();
+        }
+        private async void buttonMove(object sender, EventArgs e) 
+        {
+            int i = Convert.ToInt32(sender.ToString().Substring(35)) - 1;
+            
+            for (int p = 0; p < 10; p++) 
+            {
+                myBtn[p].Name = i.ToString();
+            }     
+                            
+            for (int count = 0; count < 10; count++)
+            {
+                myBtn[count].Click -= buttonMove;
+                myBtn[count].Click += buttonChangeNumber;
+            }
+
+            while (true ) { 
+                for (int j = 0; j < 20; j++)
+                {
+                    await Task.Delay(5);
+                    myBtn[i].Location = new Point(myBtn[i].Location.X, myBtn[i].Location.Y - 1);
+                }
+                for (int k = 0; k < 20; k++)
+                {
+                    await Task.Delay(5);
+                    myBtn[i].Location = new Point(myBtn[i].Location.X, myBtn[i].Location.Y + 1);
+                }
+            }
+        }
+
+        private void buttonChangeNumber(object sender, EventArgs e) {
+            int i = Convert.ToInt32(sender.ToString().Substring(35)) - 1;
+            Random rnd = new Random();
+            int rndValue = rnd.Next(1, 10);
+            myBtn[Convert.ToInt32(myBtn[i].Name)].Text = rndValue.ToString();
+        }
+    }
+}
